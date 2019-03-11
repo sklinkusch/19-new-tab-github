1f825c55ac7c373821b04655d4edecf593aeb0f9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-03-11 11:49:36 +0100

    added functionality for time

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,19 @@ class Time {
   constructor(docSel) {
     this.container = document.querySelector(docSel);
     this.getTime();
-    this.repeat();
+    // this.repeat();
+  }
+  getTime() {
+    const timeObject = moment();
+    const currentTime = timeObject._d;
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const hourString = hours < 10 ? `0${hours}` : `${hours}`;
+    const minString = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const outputTime = `${hourString}:${minString}`;
+    console.log(outputTime);
+    this.container = outputTime;
   }
 }
+
+const myTime = new Time("#time");
